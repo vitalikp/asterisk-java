95b75364e569eb078c947da576bfa47a3240d7cc
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-05-23 13:03:03 +0300

    * init module;

---
+++ src/main/org/asteriskjava/live/AsteriskServerListener.java
@@ -0,0 +1,31 @@
+package org.asteriskjava.live;
+
+/**
+ * You can register an AsteriskServerListener with an
+ * {@link org.asteriskjava.live.AsteriskServer} to be notified about new
+ * channels and MeetMe users.
+ * <p>
+ * Usually it is better to extend {@link AbstractAsteriskServerListener} than to
+ * implement this interface directly as additonal methods will probably be added
+ * in future versions of Asterisk-Java.
+ * 
+ * @author srt
+ * @version $Id: AsteriskServerListener.java 453 2006-06-25 09:07:23Z srt $
+ * @since 0.3
+ */
+public interface AsteriskServerListener
+{
+    /**
+     * Called whenever a new channel appears on the Asterisk server.
+     * 
+     * @param channel the new channel.
+     */
+    void onNewAsteriskChannel(AsteriskChannel channel);
+
+    /**
+     * Called whenever a user joins a {@link MeetMeRoom}.
+     * 
+     * @param user the user that joined.
+     */
+    void onNewMeetMeUser(MeetMeUser user);
+}
---
+++ src/main/org/asteriskjava/manager/event/QueueSummaryCompleteEvent.java
@@ -0,0 +1,26 @@
+package org.asteriskjava.manager.event;
+
+import org.asteriskjava.manager.action.QueueSummaryAction;
+
+/**
+ * A QueueSummaryCompleteEvent is triggered after the summary for all requested
+ * queues has been reported in response to a QueueSummaryAction.
+ * 
+ * @see QueueSummaryAction
+ * @see QueueSummaryEvent
+ * @author srt
+ * @version $Id: QueueSummaryCompleteEvent.java 573 2006-09-27 21:39:19Z srt $
+ * @since 0.3
+ */
+public class QueueSummaryCompleteEvent extends ResponseEvent
+{
+    /**
+     * Serial version identifier.
+     */
+    private static final long serialVersionUID = -5044247858568827143L;
+
+    public QueueSummaryCompleteEvent(Object source)
+    {
+        super(source);
+    }
+}

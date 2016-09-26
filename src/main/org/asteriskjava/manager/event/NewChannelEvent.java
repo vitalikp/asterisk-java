f2947f865fb5e391dc525cbc2b55df6c676e2608
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-09-26 18:07:01 +0300

    * NewChannelEvent: add missing event properties;

--- src/main/org/asteriskjava/manager/event/NewChannelEvent.java
+++ src/main/org/asteriskjava/manager/event/NewChannelEvent.java
@@ -30,11 +30,92 @@ public class NewChannelEvent extends AbstractChannelStateEvent
      */
     static final long serialVersionUID = -4503396901506287549L;
 
+    private Integer channelState;
+    private String channelStateDesc;
+
+    private String accountCode;
+
+    private String exten;
+    private String context;
+
     /**
      * @param source
      */
     public NewChannelEvent(Object source)
     {
         super(source);
+    }
+
+	public Integer getChannelState()
+	{
+		return channelState;
+	}
+
+	public void setChannelState(Integer channelState)
+	{
+		this.channelState = channelState;
+	}
+
+	public String getChannelStateDesc()
+	{
+		return channelStateDesc;
+	}
+
+	public void setChannelStateDesc(String channelStateDesc)
+	{
+		this.channelStateDesc = channelStateDesc;
+	}
+
+	/**
+     * Returns the account number that is usually used to identify the party to bill for the call.<p>
+     * Corresponds to CDR field <code>accountcode</code>.
+     * 
+     * @return the account number.
+     */
+    public String getAccountCode()
+    {
+        return accountCode;
+    }
+
+    /**
+     * Sets the account number.
+     * 
+     * @param accountCode the account number.
+     */
+    public void setAccountCode(String accountCode)
+    {
+        this.accountCode = accountCode;
+    }
+
+    /**
+     * Returns the extension.
+     */
+    public String getExten()
+    {
+        return exten;
+    }
+
+    /**
+     * Sets the extension.
+     */
+    public void setExten(String exten)
+    {
+        this.exten = exten;
+    }
+
+    /**
+     * Returns the context of the extension.
+     */
+    public String getContext()
+    {
+        return context;
+    }
+
+    /**
+     * Sets the context of the extension.
+     */
+    public void setContext(String context)
+    {
+        this.context = context;
     }
 }

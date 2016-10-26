7ab534b30ef57d6b9004f9a596f837684e1ff440
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-10-26 22:33:19 +0300

    * SocketConnectionFacade: formatting code;

--- src/main/org/asteriskjava/util/SocketConnectionFacade.java
+++ src/main/org/asteriskjava/util/SocketConnectionFacade.java
@@ -24,86 +24,86 @@ import java.net.InetAddress;
  * communication over TCP/IP sockets.<p>
  * It hides the details of the underlying I/O system used for socket
  * communication.
- * 
+ *
  * @author srt
  * @version $Id: SocketConnectionFacade.java 397 2006-05-26 12:13:32Z srt $
  */
 public interface SocketConnectionFacade
 {
-    /**
-     * Reads a line of text from the socket connection. The current thread is
-     * blocked until either the next line is received or an IOException
-     * encounters.
-     * 
-     * @return the line of text received excluding any newline character
-     * @throws IOException if the connection has been closed.
-     */
-    String readLine() throws IOException;
+	/**
+	 * Reads a line of text from the socket connection. The current thread is
+	 * blocked until either the next line is received or an IOException
+	 * encounters.
+	 *
+	 * @return the line of text received excluding any newline character
+	 * @throws IOException if the connection has been closed.
+	 */
+	String readLine() throws IOException;
 
-    /**
-     * Sends a given String to the socket connection.
-     * 
-     * @param s the String to send.
-     * @throws IOException if the String cannot be sent, maybe because the
-     *             connection has already been closed.
-     */
-    void write(String s) throws IOException;
+	/**
+	 * Sends a given String to the socket connection.
+	 *
+	 * @param s the String to send.
+	 * @throws IOException if the String cannot be sent, maybe because the
+	 *             connection has already been closed.
+	 */
+	void write(String s) throws IOException;
 
-    /**
-     * Flushes the socket connection, that is sends any buffered but yet unsent
-     * data.
-     * 
-     * @throws IOException if the connection cannot be flushed.
-     */
-    void flush() throws IOException;
+	/**
+	 * Flushes the socket connection, that is sends any buffered but yet unsent
+	 * data.
+	 *
+	 * @throws IOException if the connection cannot be flushed.
+	 */
+	void flush() throws IOException;
 
-    /**
-     * Closes the socket connection including its input and output stream and
-     * frees all associated ressources.<p>
-     * When calling close() any Thread currently blocked by a call to readLine()
-     * will be unblocked and receive an IOException.
-     * 
-     * @throws IOException if the socket connection cannot be closed.
-     */
-    void close() throws IOException;
+	/**
+	 * Closes the socket connection including its input and output stream and
+	 * frees all associated ressources.<p>
+	 * When calling close() any Thread currently blocked by a call to readLine()
+	 * will be unblocked and receive an IOException.
+	 *
+	 * @throws IOException if the socket connection cannot be closed.
+	 */
+	void close() throws IOException;
 
-    /**
-     * Returns the connection state of the socket.
-     * 
-     * @return <code>true</code> if the socket successfuly connected to a
-     *         server
-     */
-    boolean isConnected();
+	/**
+	 * Returns the connection state of the socket.
+	 *
+	 * @return <code>true</code> if the socket successfuly connected to a
+	 *         server
+	 */
+	boolean isConnected();
 
-    /**
-     * Returns the local address this socket connection.
-     * 
-     * @return the local address this socket connection.
-     * @since 0.2
-     */
-    InetAddress getLocalAddress();
+	/**
+	 * Returns the local address this socket connection.
+	 *
+	 * @return the local address this socket connection.
+	 * @since 0.2
+	 */
+	InetAddress getLocalAddress();
 
-    /**
-     * Returns the local port of this socket connection.
-     * 
-     * @return the local port of this socket connection.
-     * @since 0.2
-     */
-    int getLocalPort();
+	/**
+	 * Returns the local port of this socket connection.
+	 *
+	 * @return the local port of this socket connection.
+	 * @since 0.2
+	 */
+	int getLocalPort();
 
-    /**
-     * Returns the remote address of this socket connection.
-     * 
-     * @return the remote address of this socket connection.
-     * @since 0.2
-     */
-    InetAddress getRemoteAddress();
+	/**
+	 * Returns the remote address of this socket connection.
+	 *
+	 * @return the remote address of this socket connection.
+	 * @since 0.2
+	 */
+	InetAddress getRemoteAddress();
 
-    /**
-     * Returns the remote port of this socket connection.
-     * 
-     * @return the remote port of this socket connection.
-     * @since 0.2
-     */
-    int getRemotePort();
+	/**
+	 * Returns the remote port of this socket connection.
+	 *
+	 * @return the remote port of this socket connection.
+	 * @since 0.2
+	 */
+	int getRemotePort();
 }

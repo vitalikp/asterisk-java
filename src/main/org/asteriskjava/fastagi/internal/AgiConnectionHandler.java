916bf393b1b4b28caa4934d390dcbaa487a0c54c
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-10-28 22:56:41 +0300

    * AgiConnectionHandler: formatting code;

--- src/main/org/asteriskjava/fastagi/internal/AgiConnectionHandler.java
+++ src/main/org/asteriskjava/fastagi/internal/AgiConnectionHandler.java
@@ -32,157 +32,157 @@ import org.asteriskjava.util.SocketConnectionFacade;
  * <p>
  * It reads the request using an AgiReader and runs the AgiScript configured to
  * handle this type of request. Finally it closes the socket connection.
- * 
+ *
  * @author srt
  * @version $Id: AgiConnectionHandler.java 633 2007-03-25 17:02:18Z srt $
  */
 public class AgiConnectionHandler implements Runnable
 {
-    private static final String AJ_AGISTATUS_VARIABLE = "AJ_AGISTATUS";
-    private static final String AJ_AGISTATUS_NOT_FOUND = "NOT_FOUND";
-    private static final String AJ_AGISTATUS_SUCCESS = "SUCCESS";
-    private static final String AJ_AGISTATUS_FAILED = "FAILED";
-    private final Log logger = LogFactory.getLog(getClass());
-    private static final ThreadLocal<AgiChannel> channel = new ThreadLocal<AgiChannel>();
+	private static final String AJ_AGISTATUS_VARIABLE = "AJ_AGISTATUS";
+	private static final String AJ_AGISTATUS_NOT_FOUND = "NOT_FOUND";
+	private static final String AJ_AGISTATUS_SUCCESS = "SUCCESS";
+	private static final String AJ_AGISTATUS_FAILED = "FAILED";
+	private final Log logger = LogFactory.getLog(getClass());
+	private static final ThreadLocal<AgiChannel> channel = new ThreadLocal<AgiChannel>();
 
-    /**
-     * The socket connection.
-     */
-    private final SocketConnectionFacade socket;
+	/**
+	 * The socket connection.
+	 */
+	private final SocketConnectionFacade socket;
 
-    /**
-     * The strategy to use to determine which script to run.
-     */
-    private final MappingStrategy mappingStrategy;
+	/**
+	 * The strategy to use to determine which script to run.
+	 */
+	private final MappingStrategy mappingStrategy;
 
-    /**
-     * Creates a new AGIConnectionHandler to handle the given socket connection.
-     * 
-     * @param socket the socket connection to handle.
-     * @param mappingStrategy the strategy to use to determine which script to
-     *            run.
-     */
-    public AgiConnectionHandler(SocketConnectionFacade socket, MappingStrategy mappingStrategy)
-    {
-        this.socket = socket;
-        this.mappingStrategy = mappingStrategy;
-    }
+	/**
+	 * Creates a new AGIConnectionHandler to handle the given socket connection.
+	 *
+	 * @param socket the socket connection to handle.
+	 * @param mappingStrategy the strategy to use to determine which script to
+	 *            run.
+	 */
+	public AgiConnectionHandler(SocketConnectionFacade socket, MappingStrategy mappingStrategy)
+	{
+		this.socket = socket;
+		this.mappingStrategy = mappingStrategy;
+	}
 
-    protected AgiReader createReader()
-    {
-        return new AgiReaderImpl(socket);
-    }
+	protected AgiReader createReader()
+	{
+		return new AgiReaderImpl(socket);
+	}
 
-    protected AgiWriter createWriter()
-    {
-        return new AgiWriterImpl(socket);
-    }
+	protected AgiWriter createWriter()
+	{
+		return new AgiWriterImpl(socket);
+	}
 
-    public void run()
-    {
-        AgiChannel channel = null;
+	public void run()
+	{
+		AgiChannel channel = null;
 
-        try
-        {
-            AgiReader reader;
-            AgiWriter writer;
-            AgiRequest request;
-            AgiScript script;
+		try
+		{
+			AgiReader reader;
+			AgiWriter writer;
+			AgiRequest request;
+			AgiScript script;
 
-            reader = createReader();
-            writer = createWriter();
+			reader = createReader();
+			writer = createWriter();
 
-            request = reader.readRequest();
-            channel = new AgiChannelImpl(request, writer, reader);
+			request = reader.readRequest();
+			channel = new AgiChannelImpl(request, writer, reader);
 
-            AgiConnectionHandler.channel.set(channel);
+			AgiConnectionHandler.channel.set(channel);
 
-            script = mappingStrategy.determineScript(request);
-            if (script == null)
-            {
-                final String errorMessage;
+			script = mappingStrategy.determineScript(request);
+			if (script == null)
+			{
+				final String errorMessage;
 
-                errorMessage = "No script configured for URL '" + request.getRequestURL() + "' (script '"
-                        + request.getScript() + "')";
-                logger.error(errorMessage);
+				errorMessage = "No script configured for URL '" + request.getRequestURL() + "' (script '"
+						+ request.getScript() + "')";
+				logger.error(errorMessage);
 
-                setStatusVariable(channel, AJ_AGISTATUS_NOT_FOUND);
-                logToAsterisk(channel, errorMessage);
-            }
-            else
-            {
-                runScript(script, request, channel);
-            }
-        }
-        catch (Exception e)
-        {
-            setStatusVariable(channel, AJ_AGISTATUS_FAILED);
-            logger.error("Unexpected Exception while handling request", e);
-        }
-        finally
-        {
-            AgiConnectionHandler.channel.set(null);
-            try
-            {
-                socket.close();
-            }
-            catch (IOException e) // NOPMD
-            {
-                // swallow
-            }
-        }
-    }
+				setStatusVariable(channel, AJ_AGISTATUS_NOT_FOUND);
+				logToAsterisk(channel, errorMessage);
+			}
+			else
+			{
+				runScript(script, request, channel);
+			}
+		}
+		catch (Exception e)
+		{
+			setStatusVariable(channel, AJ_AGISTATUS_FAILED);
+			logger.error("Unexpected Exception while handling request", e);
+		}
+		finally
+		{
+			AgiConnectionHandler.channel.set(null);
+			try
+			{
+				socket.close();
+			}
+			catch (IOException e) // NOPMD
+			{
+				// swallow
+			}
+		}
+	}
 
-    private void runScript(AgiScript script, AgiRequest request, AgiChannel channel)
-    {
-        String threadName;
-        threadName = Thread.currentThread().getName();
+	private void runScript(AgiScript script, AgiRequest request, AgiChannel channel)
+	{
+		String threadName;
+		threadName = Thread.currentThread().getName();
 
-        logger.info("Begin AgiScript " + script.getClass().getName() + " on " + threadName);
-        try
-        {
-            script.service(request, channel);
-            setStatusVariable(channel, AJ_AGISTATUS_SUCCESS);
-        }
-        catch (Exception e)
-        {
-            logger.error("Exception running AgiScript " + script.getClass().getName() + " on " + threadName, e);
-            setStatusVariable(channel, AJ_AGISTATUS_FAILED);
-        }
-        logger.info("End AgiScript " + script.getClass().getName() + " on " + threadName);
-    }
+		logger.info("Begin AgiScript " + script.getClass().getName() + " on " + threadName);
+		try
+		{
+			script.service(request, channel);
+			setStatusVariable(channel, AJ_AGISTATUS_SUCCESS);
+		}
+		catch (Exception e)
+		{
+			logger.error("Exception running AgiScript " + script.getClass().getName() + " on " + threadName, e);
+			setStatusVariable(channel, AJ_AGISTATUS_FAILED);
+		}
+		logger.info("End AgiScript " + script.getClass().getName() + " on " + threadName);
+	}
 
-    private void setStatusVariable(AgiChannel channel, String value)
-    {
-        if (channel == null)
-        {
-            return;
-        }
+	private void setStatusVariable(AgiChannel channel, String value)
+	{
+		if (channel == null)
+		{
+			return;
+		}
 
-        try
-        {
-            channel.setVariable(AJ_AGISTATUS_VARIABLE, value);
-        }
-        catch (Exception e) // NOPMD
-        {
-            // swallow
-        }
-    }
+		try
+		{
+			channel.setVariable(AJ_AGISTATUS_VARIABLE, value);
+		}
+		catch (Exception e) // NOPMD
+		{
+			// swallow
+		}
+	}
 
-    private void logToAsterisk(AgiChannel channel, String message)
-    {
-        if (channel == null)
-        {
-            return;
-        }
+	private void logToAsterisk(AgiChannel channel, String message)
+	{
+		if (channel == null)
+		{
+			return;
+		}
 
-        try
-        {
-            channel.verbose(message, 1);
-        }
-        catch (Exception e) // NOPMD
-        {
-            // swallow
-        }
-    }
+		try
+		{
+			channel.verbose(message, 1);
+		}
+		catch (Exception e) // NOPMD
+		{
+			// swallow
+		}
+	}
 }

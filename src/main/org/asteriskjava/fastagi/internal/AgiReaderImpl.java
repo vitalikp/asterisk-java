60d56aac0483f2d459f86af8d3731752d509d44d
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-10-28 23:13:45 +0300

    * AgiReaderImpl: formatting code;

--- src/main/org/asteriskjava/fastagi/internal/AgiReaderImpl.java
+++ src/main/org/asteriskjava/fastagi/internal/AgiReaderImpl.java
@@ -27,78 +27,78 @@ import org.asteriskjava.util.SocketConnectionFacade;
 
 /**
  * Default implementation of the AgiReader implementation.
- * 
+ *
  * @author srt
  * @version $Id: AgiReaderImpl.java 580 2006-12-27 22:21:49Z srt $
  */
 public class AgiReaderImpl implements AgiReader
 {
-    private final SocketConnectionFacade socket;
+	private final SocketConnectionFacade socket;
 
-    public AgiReaderImpl(SocketConnectionFacade socket)
-    {
-        this.socket = socket;
-    }
+	public AgiReaderImpl(SocketConnectionFacade socket)
+	{
+		this.socket = socket;
+	}
 
-    public AgiRequest readRequest() throws IOException
-    {
-        AgiRequestImpl request;
-        String line;
-        List<String> lines;
+	public AgiRequest readRequest() throws IOException
+	{
+		AgiRequestImpl request;
+		String line;
+		List<String> lines;
 
-        lines = new ArrayList<String>();
+		lines = new ArrayList<String>();
 
-        while ((line = socket.readLine()) != null)
-        {
-            if (line.length() == 0)
-            {
-                break;
-            }
+		while ((line = socket.readLine()) != null)
+		{
+			if (line.length() == 0)
+			{
+				break;
+			}
 
-            lines.add(line);
-        }
+			lines.add(line);
+		}
 
-        request = new AgiRequestImpl(lines);
-        request.setLocalAddress(socket.getLocalAddress());
-        request.setLocalPort(socket.getLocalPort());
-        request.setRemoteAddress(socket.getRemoteAddress());
-        request.setRemotePort(socket.getRemotePort());
+		request = new AgiRequestImpl(lines);
+		request.setLocalAddress(socket.getLocalAddress());
+		request.setLocalPort(socket.getLocalPort());
+		request.setRemoteAddress(socket.getRemoteAddress());
+		request.setRemotePort(socket.getRemotePort());
 
-        return request;
-    }
+		return request;
+	}
 
-    public AgiReply readReply() throws IOException
-    {
-        AgiReply reply;
-        List<String> lines;
-        String line;
+	public AgiReply readReply() throws IOException
+	{
+		AgiReply reply;
+		List<String> lines;
+		String line;
 
-        lines = new ArrayList<String>();
+		lines = new ArrayList<String>();
 
-        line = socket.readLine();
+		line = socket.readLine();
 
-        if (line == null)
-        {
-            throw new AgiHangupException();
-        }
+		if (line == null)
+		{
+			throw new AgiHangupException();
+		}
 
-        lines.add(line);
+		lines.add(line);
 
-        // read synopsis and usage if statuscode is 520
-        if (line.startsWith(Integer.toString(AgiReply.SC_INVALID_COMMAND_SYNTAX)))
-        {
-            while ((line = socket.readLine()) != null)
-            {
-                lines.add(line);
-                if (line.startsWith(Integer.toString(AgiReply.SC_INVALID_COMMAND_SYNTAX)))
-                {
-                    break;
-                }
-            }
-        }
+		// read synopsis and usage if statuscode is 520
+		if (line.startsWith(Integer.toString(AgiReply.SC_INVALID_COMMAND_SYNTAX)))
+		{
+			while ((line = socket.readLine()) != null)
+			{
+				lines.add(line);
+				if (line.startsWith(Integer.toString(AgiReply.SC_INVALID_COMMAND_SYNTAX)))
+				{
+					break;
+				}
+			}
+		}
 
-        reply = new AgiReplyImpl(lines);
+		reply = new AgiReplyImpl(lines);
 
-        return reply;
-    }
+		return reply;
+	}
 }

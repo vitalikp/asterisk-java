a355bcfe8f70bc09b59c5499a6c0b2e6086eab80
Perevertun V.M. <vitalik.perevertun@gmail.com>
2018-10-01 16:50:47 +0300

    * EventClassMap: override “regClass” method;

--- src/main/org/asteriskjava/manager/internal/EventClassMap.java
+++ src/main/org/asteriskjava/manager/internal/EventClassMap.java
@@ -1,6 +1,7 @@
 package org.asteriskjava.manager.internal;
 
 import org.asteriskjava.manager.event.ManagerEvent;
+import org.asteriskjava.manager.event.UserEvent;
 
 class EventClassMap extends ClassMap<ManagerEvent>
 {
@@ -9,5 +10,13 @@ class EventClassMap extends ClassMap<ManagerEvent>
 	public EventClassMap()
 	{
 		super("Event", ManagerEvent.class, new Class[]{Object.class});
+	}
+
+	protected void regClass(String type, Class<? extends ManagerEvent> cls)
+	{
+		if (UserEvent.class.isAssignableFrom(cls) && !type.startsWith("userevent"))
+			type = "userevent" + type;
+
+		super.regClass(type, cls);
 	}
 }
